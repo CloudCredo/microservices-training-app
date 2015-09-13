aac7733ad6600e0d56a8c4328f8d01fe380b62e1
Gareth Clay <gareth@cloudcredo.com>
2015-09-13 13:28:07 +0100

    Successful roundtripping
    [#102012958]

--- src/main/resources/static/js/aggregated-feedback.js
+++ src/main/resources/static/js/aggregated-feedback.js
@@ -1,17 +1,99 @@
 angular.module('aggregatedFeedback', ['feedbackService'])
+  //.factory('AggregatedFeedbackModel', function () {
+  //  var feedback = {
+  //    happiness: [0, 0, 0, 0, 0],
+  //    learning: [0, 0, 0]
+  //  };
+  //
+  //  function totalVotes(votes) {
+  //    return votes.reduce(function (sum, value) {
+  //      return sum + value;
+  //    })
+  //  }
+  //
+  //  function percentage(votes, level) {
+  //    return votes[level] * 100 / totalVotes(votes)
+  //  }
+  //
+  //  return {
+  //    setFeedback: function(feedback) {
+  //      this.feedback = feedback;
+  //    },
+  //
+  //    happinessLevelPercentage: function(level) {
+  //      return percentage(feedback.happiness, level);
+  //    },
+  //
+  //    learningLevelPercentage: function(level) {
+  //      return percentage(feedback.learning, level);
+  //    }
+  //  }
+  //})
+
   .value('AggregatedFeedbackModel', {
-    feedback: {
-      happiness: [0, 0, 0, 0, 0],
-      learning: [0, 0, 0]
-    }
+    happinessLevels: [
+      {
+        description: 'This sucks!',
+        votes: 0
+      },
+      {
+        description: 'A bit miserable',
+        votes: 0
+      },
+      {
+        description: 'Meh',
+        votes: 0
+      },
+      {
+        description: 'Pretty good',
+        votes: 0
+      },
+      {
+        description: 'Awesome!',
+        votes: 0
+      }
+    ],
+    learningLevels: [
+      {
+        description: 'Nothing!',
+        votes: 0
+      },
+      {
+        description: 'A little',
+        votes: 0
+      },
+      {
+        description: 'Soaking up new knowledge like a sponge!',
+        votes: 0
+      }
+    ],
+    totalVotes: 0
   })
 
   .controller('AggregatedFeedbackController', function($scope, $interval, FeedbackService, AggregatedFeedbackModel) {
     function updateAggregatedFeedback() {
-      AggregatedFeedbackModel.feedback = FeedbackService.getAggregatedFeedback();
+      function totalVotes(feedback) {
+        return feedback.happiness.reduce(function (sum, value) {
+          return sum + value;
+        })
+      }
+
+      function updateModelVotes(modelArray, newLevels) {
+        console.log("Model array: " + modelArray);
+        console.log("newLevels: " + newLevels);
+        for (var i = 0; i < modelArray.length; i++) {
+          modelArray[i].votes = newLevels[i];
+        }
+      }
+
+      FeedbackService.getAggregatedFeedback().$promise.then(function (feedback) {
+        updateModelVotes(AggregatedFeedbackModel.happinessLevels, feedback.happiness);
+        updateModelVotes(AggregatedFeedbackModel.learningLevels, feedback.learning);
+        AggregatedFeedbackModel.totalVotes = totalVotes(feedback);
+      });
     }
 
-    $scope.aggregatedFeedback = AggregatedFeedbackModel.feedback;
+    $scope.aggregatedFeedback = AggregatedFeedbackModel;
 
     $interval(updateAggregatedFeedback, 3000)
   });
