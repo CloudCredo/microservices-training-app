49bed3190bb55e07fefb15f9465f129e4c57286a
Gareth Clay <gareth@cloudcredo.com>
2015-10-01 09:15:49 +0100

    Add request metadata aggregator
    [#102852288]

---
+++ request-subscriber/lib/metadata_aggregator.rb
@@ -0,0 +1,22 @@
+class MetadataAggregator
+
+  def initialize(redis)
+    @redis = redis
+    @aggregated_metadata = {}
+  end
+
+  def aggregate(request_metadata)
+    request_metadata = JSON.parse(request_metadata)
+    increment(request_metadata['method'], request_metadata['path'])
+  end
+
+  private
+
+  attr_reader :aggregated_metadata, :redis
+
+  def increment(method, path)
+    key = "aggregatedMetadata:#{method}:#{path}"
+    redis.incr(key)
+  end
+
+end
