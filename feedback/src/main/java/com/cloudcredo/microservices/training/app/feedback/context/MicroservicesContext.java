5fc1cbc5a3692b2efd43ed4b7bcb10a20c6416c0
Christopher Hedley <chris@cghsystems.net>
2015-10-04 15:46:01 +0100

    Inject Rest Template to get distributed tracing running

--- feedback/src/main/java/com/cloudcredo/microservices/training/app/feedback/context/MicroservicesContext.java
+++ feedback/src/main/java/com/cloudcredo/microservices/training/app/feedback/context/MicroservicesContext.java
@@ -1,21 +1,28 @@
 package com.cloudcredo.microservices.training.app.feedback.context;
 
 import com.cloudcredo.microservices.training.app.feedback.rest.RestProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
 @Configuration
 public class MicroservicesContext {
+
+  @Autowired(required = true)
+  private RestTemplate restTemplate;
+
   @Bean
   public RestProxy questionsService(@Value("${vcap.services.questions.credentials.url}") String url) {
-    return new RestProxy(URI.create("http://" + url));
+    return new RestProxy(URI.create("http://" + url), restTemplate);
   }
 
   @Bean
   public RestProxy myMicroservice(@Value("${MY_MICROSERVICE_URL}") String url) {
-    return new RestProxy(URI.create("http://" + url));
+    return new RestProxy(URI.create("http://" + url), restTemplate);
   }
 }
